eda766a21e1f0c72ca741a04d68220f572ce4978
lnation <thisusedtobeanemail@gmail.com>
2019-11-17 09:29:34 +0800

    bump version for release

--- lib/Cache/Memcached/Managed.pm
+++ lib/Cache/Memcached/Managed.pm
@@ -2,7 +2,7 @@ package Cache::Memcached::Managed;
 
 # Make sure we have version info for this module
 
-$VERSION= '0.25';
+$VERSION= '0.26';
 
 # Make sure we're as strict as possible
 # With as much feedback that we can get
